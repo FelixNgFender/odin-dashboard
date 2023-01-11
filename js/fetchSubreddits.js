d6985ec5186bd09b45e975c465da261f9070f734
Felix Nguyen <ngthinh302@gmail.com>
2023-01-11 01:36:12 -0500

    Add favorite subreddits section

--- js/fetchSubreddits.js
+++ js/fetchSubreddits.js
@@ -1,32 +1,31 @@
-const subredditCount = 4;
-//const accessToken = 'YOUR_ACCESS_TOKEN_HERE';
+const subreddits = [
+  'webdev',
+  'computerscience',
+  'cscareerquestions',
+  'manga'
+];
 
-fetch('https://oauth.reddit.com/api/v1/me', {
-  headers: {
-    'Authorization': `bearer ${accessToken}`,
-    'User-Agent': 'MyApp/1.0'
-  }
-})
-  .then(response => response.json())
-  .then(data => {
-    const subredditGrid = document.getElementById('mainContent-subredditGrid');
-    const subreddits = data.data.subreddit;
-    for (let i = 0; i < subredditCount; i++) {
-      const subreddit = subreddits[i];
-      // Create the subreddit box
-      const subredditBox = document.createElement('div');
-      subredditBox.classList.add('mainContent-subredditBox');
-      // Add the subreddit image
-      const subredditImage = document.createElement('img');
-      subredditImage.classList.add('mainContent-subredditImage');
-      subredditImage.src = subreddit.icon_img;
-      subredditBox.appendChild(subredditImage);
-      // Add the subreddit name
-      const subredditName = document.createElement('div');
-      subredditName.classList.add('mainContent-subredditName');
-      subredditName.textContent = subreddit.display_name;      
-      subredditBox.appendChild(subredditName);
-      // Add the subreddit box to the grid
-      subredditGrid.appendChild(subredditBox);
-    }
-  });
+const fetchSubreddit = (subreddit) => {
+  fetch(`https://www.reddit.com/r/${subreddit}/about.json`)
+    .then(response => response.json())
+    .then(data => {
+      const subredditData = data.data;
+      const subredditGrid = document.getElementById("mainContent-favoriteGrid");
+      const subredditItem = document.createElement("li");
+      const subredditLink = document.createElement("a");
+      const subredditIcon = document.createElement("img");
+      subredditIcon.src = subredditData.icon_img;
+      subredditLink.href = `https://www.reddit.com/r/${subreddit}`;
+      subredditLink.textContent = `r/${subreddit}`;
+      subredditLink.target = "_blank";
+      subredditLink.rel = "noopener noreferrer";
+      subredditItem.classList.add("mainContent-favoriteItem");
+      subredditIcon.classList.add("mainContent-favoriteIcon");
+      subredditLink.classList.add("mainContent-favoriteLink");
+      subredditItem.appendChild(subredditIcon);
+      subredditItem.appendChild(subredditLink);
+      subredditGrid.appendChild(subredditItem);
+    });
+}
+
+subreddits.forEach(subreddit => fetchSubreddit(subreddit));
--- js/fetchTrendingPosts.js
+++ js/fetchTrendingPosts.js
@@ -6,13 +6,18 @@ fetch(
   .then((response) => response.json())
   .then((data) => {
     const postList = document.getElementById("mainContent-trendingPostGrid");
-    console.log(data);
     for (let i = 0; i < postCount; i++) {
       const post = data.data.children[i];
       const postItem = document.createElement("li");
       const postLink = document.createElement("a");
       const postThumbnail = document.createElement("img");
+      const postSubreddit = document.createElement("a");
       postLink.href = `https://reddit.com${post.data.permalink}`;
+      postSubreddit.href = `https://reddit.com/${post.data.subreddit_name_prefixed}`;
+      postSubreddit.textContent = post.data.subreddit_name_prefixed;
+      postSubreddit.target = "_blank";
+      postSubreddit.rel = "noopener noreferrer";
+      postSubreddit.classList.add("mainContent-trendingPostSubreddit");
       postLink.textContent = post.data.title;
       postThumbnail.src = post.data.thumbnail;
       postLink.target = "_blank";
@@ -21,6 +26,7 @@ fetch(
       postThumbnail.classList.add("mainContent-trendingPostThumbnail");
       postLink.classList.add("mainContent-trendingPostLink");
       postItem.appendChild(postThumbnail);
+      postItem.appendChild(postSubreddit);
       postItem.appendChild(postLink);
       postList.appendChild(postItem);
     }
